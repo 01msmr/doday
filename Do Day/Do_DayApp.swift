17f97a2aa997057acd0b067aca17e0324c5ff5e5
Ulrich Maasmeier <u@msmr.co>
2026-04-19 14:06:06 +0200

    Initial Commit

---
+++ Do Day/Do_DayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Do_DayApp.swift
+//  Do Day
+//
+//  Created by Uli on 19.04.26.
+//
+
+import SwiftUI
+
+@main
+struct Do_DayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
